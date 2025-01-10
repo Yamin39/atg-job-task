f740d5ba8f6d5bb49da8299a683412516002fcf6
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-01-10 23:27:23 +0600

    project initialized

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-medium text-red-600">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "ibm-plex-sans": `"IBM Plex Sans", serif`,
+      },
+    },
+  },
+  plugins: [],
+};
