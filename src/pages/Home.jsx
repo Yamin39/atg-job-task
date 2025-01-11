a43051053e780be0b456f16796c9ac77d2c14c19
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-01-12 00:50:17 +0600

    added post button for small device

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import penIcon from "../assets/icons/pen.svg";
 import Banner from "../components/Banner/Banner";
 import Filter from "../components/Filter/Filter";
 import Navbar from "../components/Navbar/Navbar";
@@ -12,6 +13,13 @@ const Home = () => {
       <Banner isJoinGroup={isJoinGroup} setIsJoinGroup={setIsJoinGroup}></Banner>
       <Filter isJoinGroup={isJoinGroup} setIsJoinGroup={setIsJoinGroup}></Filter>
       <Posts></Posts>
+
+      <button
+        className="size-[54px] flex 2md:hidden flex-col rounded-full fixed bottom-[18px] right-[18px] justify-center items-center shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),0px_2px_4px_0px_rgba(0,0,0,0.18)]"
+        style={{ background: "linear-gradient(180deg, rgb(255, 92, 92), rgb(240, 86, 138) 100%)" }}
+      >
+        <img src={penIcon} className="size-6" alt="Pen Icon" />
+      </button>
     </div>
   );
 };
